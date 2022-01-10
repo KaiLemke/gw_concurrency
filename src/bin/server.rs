d30f80a917d4ab482aab580153e47032548222d8
Kai Lemke <kai_lemke@genua.de>
2022-01-10 15:59:23 +0100

    Use warp as server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,21 +1,10 @@
-use std::{net::TcpListener, process};
+use warp::Filter;
 
-use opcode::server::handle_connection;
+use opcode::server::GREETING;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8000").unwrap_or_else(|e| {
-        eprintln!("Could not start server: {:#?}", e);
-        process::exit(1)
-    });
+#[tokio::main]
+async fn main() {
+    let greeting = warp::path!().map(|| GREETING);
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap_or_else(|e| {
-            eprintln!("Could not get stream: {:#?}", e);
-            process::exit(1);
-        });
-        handle_connection(stream).unwrap_or_else(|e| {
-            eprintln!("Reading the stream failed: {:#?}", e);
-            process::exit(1);
-        });
-    }
+    warp::serve(greeting).run(([127, 0, 0, 1], 8000)).await;
 }
--- src/server.rs
+++ src/server.rs
@@ -1,22 +1,4 @@
-use std::io::Read;
-use std::io::{self, Write};
-use std::net::TcpStream;
-
-pub fn handle_connection(mut stream: TcpStream) -> Result<(), io::Error> {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer)?;
-
-    let response = format!(
-        "HTTP/1.1 200 OK\r\r\nContent-Length: {}\r\n\r\n{}",
-        GREETING.len(),
-        GREETING
-    );
-
-    stream.write(response.as_bytes())?;
-    stream.flush()
-}
-
-const GREETING: &str = r#"Welcome to opcode server!
+pub const GREETING: &str = r#"Welcome to opcode server!
 
 You can send me an intcode, i.e. a list of integers like '(1,0,0,3,99)'.
 
