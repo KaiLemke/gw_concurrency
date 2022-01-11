ef6abfa9ec716e9b183f6a95a6892f8e87a2143c
Kai Lemke <kai_lemke@genua.de>
2022-01-11 11:28:58 +0100

    Remove greeting for now
    It's not used yet.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -5,18 +5,6 @@ use warp::{ws::Message, Filter};
 pub mod handler;
 pub mod ws;
 
-pub const GREETING: &str = r#"Welcome to opcode server!
-
-You can send me an intcode, i.e. a list of integers like '(1,0,0,3,99)'.
-
-Index 0 is an opcode of the following:
-    -  1 - add     : Adds together numbers read from two positions and stores a result in a third position.
-    -  2 - multiply: Does the same as 1 but with multiplication.
-    - 99 - exit    : Exits the program, i.e. closes the connection immediately.
-
-If no exit opcode is sent, I will accept further opcodes.
-"#;
-
 #[derive(Debug, Clone)]
 pub struct Client {
     pub client_id: String,
