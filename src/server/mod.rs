387ae438e05d3f6ad1b7bda1770bfc524cb40190
Kai Lemke <kai_lemke@genua.de>
2022-01-11 12:00:11 +0100

    Add documentation

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,3 +1,5 @@
+//! OpCode Server
+
 use std::{collections::HashMap, convert::Infallible, result::Result, sync::Arc};
 use tokio::sync::{mpsc, Mutex};
 use warp::{ws::Message, Filter};
@@ -5,14 +7,19 @@ use warp::{ws::Message, Filter};
 pub mod handler;
 pub mod ws;
 
+/// A Client
 #[derive(Debug, Clone)]
 pub struct Client {
+    /// The client's UUID
     pub client_id: String,
+    /// Communication channel sender end
     pub sender: Option<mpsc::UnboundedSender<Result<Message, warp::Error>>>,
 }
 
+/// A list of registered clients keyed by UUIDs
 pub type Clients = Arc<Mutex<HashMap<String, Client>>>;
 
+/// `warp::Filter` for handling clients
 pub fn with_clients(
     clients: Clients,
 ) -> impl Filter<Extract = (Clients,), Error = Infallible> + Clone {
