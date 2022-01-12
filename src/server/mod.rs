f52c43ae010ecdd2741a5c305ea5b4f39b5e7583
Kai Lemke <kai_lemke@genua.de>
2022-01-12 17:18:07 +0100

    Use bounded channels for server
    Using unbounded channels may open doors for DOS attacks.
    Switch to bounded channels.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -7,13 +7,16 @@ use warp::{ws::Message, Filter};
 pub mod handler;
 pub mod ws;
 
+/// The buffer size for `mpsc::channel`s for client connections
+pub const CLIENT_CONN_SIZE: usize = 42;
+
 /// A Client
 #[derive(Debug, Clone)]
 pub struct Client {
     /// The client's UUID
     pub client_id: String,
     /// Communication channel sender end
-    pub sender: Option<mpsc::UnboundedSender<Result<Message, warp::Error>>>,
+    pub sender: Option<mpsc::Sender<Result<Message, warp::Error>>>,
 }
 
 /// A list of registered clients keyed by UUIDs
