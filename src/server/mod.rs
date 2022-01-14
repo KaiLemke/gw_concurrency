4c6f9c3c38bb3a253b1ac9f72640628bd06f78ce
Kai Lemke <kai_lemke@genua.de>
2022-01-14 16:27:31 +0100

    Add client module to server
    Move the client related stuff to a separate module and re-export types.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,14 +1,13 @@
 //! OpCode Server
 
-use std::{collections::HashMap, result::Result, sync::Arc};
-use tokio::sync::{mpsc, Mutex};
-use warp::ws::Message;
-
 use crate::command::HELP;
 
+pub mod client;
 pub mod filter;
 pub mod handler;
 pub mod ws;
+
+pub use client::{Client, Clients};
 
 /// The buffer size for `mpsc::channel`s for client connections
 pub const CLIENT_CONN_SIZE: usize = 42;
@@ -22,15 +21,3 @@ pub fn greeting() -> String {
         HELP[1], HELP[2], HELP[3], HELP[4], HELP[5], HELP[6]
     )
 }
-
-/// A Client
-#[derive(Debug, Clone)]
-pub struct Client {
-    /// The client's UUID
-    pub client_id: String,
-    /// Communication channel sender end
-    pub sender: Option<mpsc::Sender<Result<Message, warp::Error>>>,
-}
-
-/// A list of registered clients keyed by UUIDs
-pub type Clients = Arc<Mutex<HashMap<String, Client>>>;
